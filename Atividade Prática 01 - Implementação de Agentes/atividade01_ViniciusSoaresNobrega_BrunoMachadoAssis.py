28e27704e24b715defa26a4e8078f99d72fbdefc
Vinicius <viniipvp@gmail.com>
2024-08-02 21:46:16 -0300

    final commit

--- Atividade Prática 01 - Implementação de Agentes/atividade01_ViniciusSoaresNobrega_BrunoMachadoAssis.py
+++ Atividade Prática 01 - Implementação de Agentes/atividade01_ViniciusSoaresNobrega_BrunoMachadoAssis.py
@@ -1,3 +1,4 @@
+import random
 class Ambiente:
     def __init__(self):
         self.grid = [[0,0,0],[0,"x","x"], [0,0,1]]
@@ -8,12 +9,14 @@ class Ambiente:
 
 
 class Agente:
+
     def __init__(self, ambiente):
         self.ambiente = ambiente
-        self.posicao = [0,1]
+        self.posicao = [0,0]
 
-    def mover_agente(self,direcao):
-        return 0
+    def verificar_objetivo(self):
+        x, y = self.posicao
+        return self.ambiente.grid[x][y] == 1
 
     def movimentos_possiveis(self):
         movimentos = []
@@ -28,9 +31,35 @@ class Agente:
         if y < len(grid[0]) - 1 and grid[x][y+1] != 'x':
             movimentos.append('direita')
         return movimentos
+    
+    def mover_agente(self):
+        movimentos_possiveis = self.movimentos_possiveis()
+        if movimentos_possiveis:
+            direcao = random.choice(movimentos_possiveis)
+            x, y = self.posicao
+            if direcao == 'cima':
+                self.posicao = [x-1, y]
+            elif direcao == 'baixo':
+                self.posicao = [x+1, y]
+            elif direcao == 'esquerda':
+                self.posicao = [x, y-1]
+            elif direcao == 'direita':
+                self.posicao = [x, y+1]
+            print(f"Agente movido para {self.posicao} na direção {direcao}")
+        else:
+            print(f"Não há movimentos possíveis a partir da posição {self.posicao}")
         
 ambiente = Ambiente()
 agente = Agente(ambiente)
 
 ambiente.mostrar_ambiente()
-print(agente.movimentos_possiveis())
+print("Posição inicial do agente:", agente.posicao)
+
+movimentos_maximos = 20
+for _ in range(movimentos_maximos):
+    agente.mover_agente()
+    if agente.verificar_objetivo():
+        print("O agente alcançou o objetivo!")
+        break
+
+print("Posição final do agente:", agente.posicao)
